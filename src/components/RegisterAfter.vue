88c4154e529764707e6491d579d78008d84c41f5
killrealpp <kaysarovkirill5@gmail.com>
2025-04-22 18:44:00 +0300

    full register

--- src/components/RegisterAfter.vue
+++ src/components/RegisterAfter.vue
@@ -2,14 +2,79 @@
     <div class="register-after">
         <div class="container">
             <div class="register-after__inner">
-                пирвет 
+                <div class="logo">
+                    <img class="logo__img" src="/logo.svg" alt="logo image">
+                </div>
+                <div class="welcome-form">
+                    <h4 class="welcome-form__head">
+                        Добро пожаловать на обучающий курс «Тим Лид» от компании БАРС Груп!
+                    </h4>
+                    <div class="welcome-form__footer">
+                        <p class="welcome-form__footer-text">Здесь вы освоите ключевые управленческие навыки: тайм-менеджмент, лидерство, управление задачами и командой, а также подходы к эффективной коммуникации  и принятию решений.</p>
+                        <p class="welcome-form__footer-text">Обучение построено поэтапно — следуйте  за заданиями и двигайтесь к следующему уровню, удачи!</p>
+                    </div>
+                </div>
+                <my-button 
+                    class="register-btn"
+                    @click="router.push('/home')"
+                >Начать обучение
+                </my-button>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
 </script>
 
 <style lang="scss">
+.register-after {
+    padding: 71px 0 61px;
+    background-color: #fff;
+    &__inner {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 53px;
+    }
+}
+.welcome-form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 24px;
+
+        &__head{
+            max-width: 320px;
+            width: 100%;
+            font-weight: 500;
+            font-size: 17px;
+            color: #000;
+        }
+
+        &__footer{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 10px;
+
+            &-text{
+                text-align: center;
+            }
+        }
+}
+
+.register-btn{
+    padding: 8px 0;
+    max-width: 257px;
+}
+
+.logo {
+    &__img {
+        max-width: 122px;
+        width: 100%;
+    }
+}
 </style>
---
+++ src/components/blocks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="blocks">
+        <div class="container">
+            <div class="blocks__inner">
+                <div class="blocks-item">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
