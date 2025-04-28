b9aae96646b4acf367778e3dfe3a4efdc5e7ddac
killrealpp <kaysarovkirill5@gmail.com>
2025-04-28 22:19:08 +0300

    full project8

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -17,7 +17,9 @@ const registerStatus = ref(false);
 const loading = ref(true);
 
 onMounted(async ()=>{
-    console.log('tot chto ', window.Telegram?.WebApp?.initDataUnsafe?.user?.id)
+    // const userId = window.Telegram?.WebApp?.initDataUnsafe;
+    // console.log('totot', userId)
+    // console.log('tot chto ', window.Telegram?.WebApp?.initDataUnsafe?.user?.id)
     const userId = window.Telegram?.WebApp?.initDataUnsafe?.user?.id
     // const userId = 123457
     console.log(' tot',userId)
